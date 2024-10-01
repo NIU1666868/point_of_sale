252e004babcf0d12d13f541a7b8b8d50e583ada1
meri <1392328@uab.cat>
2024-10-01 21:12:04 +0200

    Empezado metodo greedy

--- src/pos_creditcard/ChangeMaker.java
+++ src/pos_creditcard/ChangeMaker.java
@@ -4,7 +4,7 @@ import java.util.HashMap;
 abstract class ChangeMaker {
   protected HashMap<Double, Integer> denominations = new HashMap<>();; //Key=Cash, Value=Quantity
   protected double[] denominationCash = {0.01, 0.02, 0.05, 0.10, 0.20, 0.50, 1.00, //Default cash
-                                        2.00, 5.00, 10.00, 20.00, 50.00, 100.00};
+                                        2.00, 5.00, 10.00, 20.00, 50.00};
 
   ChangeMaker() {
     for (int i = 0; i < denominationCash.length; i++) {
--- src/pos_creditcard/GreedyChangeMaker.java
+++ src/pos_creditcard/GreedyChangeMaker.java
@@ -1,8 +1,21 @@
 package pos_creditcard;
 
-public class GreedyChangeMaker {
+public class GreedyChangeMaker{
 
-  public void change(double moneyChange)  {
+  public void change(double moneyChange) {
+
+    while (moneyChange != 0)
+    {
+      for (int i = denominationCash.length() - 1; i >= 0; i--)
+      {
+        if (denominationCash[i] <= moneyChange && denominations[i].value != 0)
+        {
+          moneyChange = moneyChange - denominationCash[i];
+          updateCashRegister(denominationCash[i]);
+          break;
+        }
+      }
+    }
 
   }
 }
